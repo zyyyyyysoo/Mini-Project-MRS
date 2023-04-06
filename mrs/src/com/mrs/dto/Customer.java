98d1e39bab747133f7c2bd551574f30788d0b42c
yjh0816 <2543kt@naver.com>
2023-04-06 13:31:37 +0900

    feat: Add getter and setter to Customer dto

--- mrs/src/com/mrs/dto/Customer.java
+++ mrs/src/com/mrs/dto/Customer.java
@@ -29,6 +29,53 @@ public class Customer {
 		this.email = email;
 		this.age = age;
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCust_id() {
+		return cust_id;
+	}
+	public void setCust_id(String cust_id) {
+		this.cust_id = cust_id;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public ArrayList<Reservation> getReservation() {
+		return reservation;
+	}
+	public void setReservation(ArrayList<Reservation> reservation) {
+		this.reservation = reservation;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", cust_id=" + cust_id + ", phone=" + phone + ", email="
+				+ email + ", age=" + age + ", reservation=" + reservation + "]";
+	}
 	
 	
 }
